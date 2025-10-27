9bdfee0758f521430f17193b743a1942a282d9d7
zanjero <zanjero3333@gmail.com>
2025-10-27 23:24:20 +0800

    removed interpolation

--- main.tf
+++ main.tf
@@ -24,10 +24,10 @@ terraform {
 data "aws_caller_identity" "current" {} 
  
 locals { 
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}" 
-  account_id  = "${data.aws_caller_identity.current.account_id}" 
+  name_prefix = "${split("/", "data.aws_caller_identity.current.arn)[1]}"  # "${split("/", "${data.aws_caller_identity.current.arn}")[1]}" 
+  account_id  = data.aws_caller_identity.current.account_id 
 } 
  
 resource "aws_s3_bucket" "s3_tf" { 
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}" 
+  bucket = format("%s-s3-tf-bkt-%s", local.name_prefix, tostring(local.account_id)) # "${local.name_prefix}-s3-tf-bkt-${local.account_id}" 
 }
