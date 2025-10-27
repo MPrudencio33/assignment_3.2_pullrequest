f62e75d86ebf362290fa699a757e6dd64f773ca2
zanjero <zanjero3333@gmail.com>
2025-10-27 23:43:56 +0800

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-provider "aws" { 
-  region = "us-east-1" 
-} 
- 
-terraform { 
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
   # Specify the required Terraform version
   required_version = ">= 1.0.0"
 
@@ -14,20 +14,20 @@ terraform {
     }
   }
 
-  backend "s3" { 
-    bucket = "sctp-ce11-tfstate" 
-    key    = "marlon3-2.tfstate"  #Change this 
-    region = "us-east-1" 
-  } 
-} 
- 
-data "aws_caller_identity" "current" {} 
- 
-locals { 
-  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]  # "${split("/", "${data.aws_caller_identity.current.arn}")[1]}" 
-  account_id  = data.aws_caller_identity.current.account_id 
-} 
- 
-resource "aws_s3_bucket" "s3_tf" { 
-  bucket = format("%s-s3-tf-bkt-%s", local.name_prefix, tostring(local.account_id)) # "${local.name_prefix}-s3-tf-bkt-${local.account_id}" 
+  backend "s3" {
+    bucket = "sctp-ce11-tfstate"
+    key    = "marlon3-2.tfstate" #Change this 
+    region = "us-east-1"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1] # "${split("/", "${data.aws_caller_identity.current.arn}")[1]}" 
+  account_id  = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket = format("%s-s3-tf-bkt-%s", local.name_prefix, tostring(local.account_id))  # "${local.name_prefix}-s3-tf-bkt-${local.account_id}" 
 }
